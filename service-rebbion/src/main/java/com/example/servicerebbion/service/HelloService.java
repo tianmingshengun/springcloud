14f699d9545558e9c4fb526b74e0bb96f54b2447
张敬文 <zhangjingwen@bianlimall.com>
2018-06-27 18:30:40 +0800

    增加服务消费者

---
+++ service-rebbion/src/main/java/com/example/servicerebbion/service/HelloService.java
@@ -0,0 +1,15 @@
+package com.example.servicerebbion.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class HelloService {
+    @Autowired
+    RestTemplate restTemplate;
+
+    public String hiService(String name){
+        return restTemplate.getForObject("http://FIRSTCLIENT/hi?name="+name, String.class);
+    }
+}
